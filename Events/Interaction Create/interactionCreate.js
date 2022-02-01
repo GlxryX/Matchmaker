1134d19eb283b166210107a0a07e26242631e131
FallenHero <77895395+GlxryX@users.noreply.github.com>
2022-02-01 01:36:54 -0500

    Update interactionCreate.js

--- Events/Interaction Create/interactionCreate.js
+++ Events/Interaction Create/interactionCreate.js
@@ -3,9 +3,6 @@ const client = require("../../index.js");
 
 client.on("interactionCreate", async (interaction) => {
   if (interaction.isCommand()) {
-    // await interaction.deferReply(({
-    //     ephemeral: false,
-    // })).catch(() => {});
     const cmd = client.slashCommands.get(interaction.commandName);
     if (!cmd)
       return interaction.reply({
@@ -14,32 +11,6 @@ client.on("interactionCreate", async (interaction) => {
 
     const guild = client.guilds.cache.get(interaction.guildId);
     const args = [];
-
-    // const cmd = client.commands.get(interaction.commandName);
-    // if (cmd) {
-    //   if (cmd.cooldown) {
-    //     const cooldwn =
-    //       cooldown.get(`${cmd.name}${interaction.user.id}`) - Date.now();
-    //     const mth = Math.floor(cooldwn / 1000) + "";
-    //     if (cooldown.has(`${cmd.name}${interaction.user.id}`))
-    //       return interaction.reply({
-    //         embeds: [
-    //           new Discord.MessageEmbed()
-    //             .setColor("RED")
-    //             .setDescription(
-    //               `You are on a cooldown. Try this command again in \`${
-    //                 mth.split(".")[0]
-    //               }\` seconds`
-    //             ),
-    //         ],
-    //         ephemeral: true,
-    //       });
-    //     cooldown.set(
-    //       `${cmd.name}${interaction.user.id}`,
-    //       Date.now() + cmd.cooldown
-    //     );
-    //   }
-    // }
 
     for (let option of interaction.options.data) {
       if (option.type === "SUB_COMMAND") {
@@ -53,10 +24,7 @@ client.on("interactionCreate", async (interaction) => {
     if (
       interaction.channel == null // If the interaction is in a DM
     ) {
-        const embed = new Discord.MessageEmbed()
-          .setColor("BLACK")
-          .setDescription("This command cannot be used in a Direct Message");
-        return interaction.reply({ embeds: [embed], ephemeral: true });
+        return interaction.reply({ content: "This command cannot be used in a Direct Message", ephemeral: true });
     } else {
       interaction.member = interaction.guild.members.cache.get(
         interaction.user.id
