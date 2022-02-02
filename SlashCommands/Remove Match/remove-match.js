3e8dcb52f76c621945093b0689194153397a8c42
GlxryX <mihirnbalsara@gmail.com>
2022-02-01 22:32:31 -0500

    Create remove-match.js

---
+++ SlashCommands/Remove Match/remove-match.js
@@ -0,0 +1,31 @@
+const schema = require("../../models/match");
+
+module.exports = {
+  name: "remove-match",
+  description: "Remove a sent match",
+
+  async execute(client, interaction, Discord) {
+    let check = await schema.findOne({ AuthorID: interaction.member.id });
+    if (check !== null) {
+      let matchFound = await schema.findOne({ AuthorID: check.MatchID });
+      if (matchFound) {
+        return interaction.reply({
+          content: `You are already matched with <@${matchFound.AuthorID}> and therefore can't remove your match.`,
+          ephemeral: true,
+        });
+      } else {
+        await schema.deleteOne({ ChannelID: interaction.channel.id }).then(() => {
+          interaction.reply({
+            content: `Your match has successfully been removed.`,
+            ephemeral: true,
+          });
+        });
+      }
+    } else {
+        return interaction.reply({
+            content: `You currently have no requested match.`,
+            ephemeral: true,
+        });
+    }
+  },
+};
